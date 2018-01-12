947ea2011095453e7089905d4cbe78b4ff78a031
davidwhogg <david.hogg@nyu.edu>
2018-01-12 07:04:32 -0500

    tiny tweaks

--- code/pca.py
+++ code/pca.py
@@ -14,10 +14,8 @@ issues:
 - Search for HACK and MAGIC and QUESTION
 '''
 
-import math
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams["font.sans-serif"] = "Helvetica" # ZOMG, Bedell!
 from astropy.table import Table
 
 if __name__ == "__main__":
